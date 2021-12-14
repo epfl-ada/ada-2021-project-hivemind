7b9df0e7c510543b59fce7f1b5474c300fec4c60
m43 <frano.rajich@gmail.com>
2021-12-14 14:38:36 +0100

    Update tweets-bert

--- scripts/scitas_cluster_init.sh
+++ scripts/scitas_cluster_init.sh
@@ -18,8 +18,12 @@ module load mvapich2
 module load py-tensorflow
 
 ## To create a Conda environment (should be done only once)
-# conda create --name ada -y
-# pip install tensorflow_hub tensorflow_estimator==2.3.0 sentencepiece jupyter pyarrow --user
+# conda create --name ada python=3.7 -y
+# conda activate ada
+# conda clean -ay
+# conda install pip -y
+# python -m pip install --upgrade pip
+# pip install tensorflow_hub tensorflow_estimator==2.3.0 astunparse numpy gast keras_preprocessing scipy pytz importlib-metadata sentencepiece jupyter pyarrow tensorboard tqdm termcolor decorator mpi4py jupyter google_pasta opt_einsum wrapt pandas dask[complete] sklearn --prefix="$CONDA_PREFIX"
 
 conda activate ada
 cd /scratch/izar/rajic/ada
@@ -28,7 +32,7 @@ cd /scratch/izar/rajic/ada
 python -c 'import tensorflow as tf; print("Num GPUs Available: ", len(tf.config.list_physical_devices("GPU")))'
 
 # Test if all needed modules can be loaded
-python -c 'import numpy as np; import pandas as pd; import tensorflow as tf; from tensorflow.keras.layers import Dense, Input; from tensorflow.keras.optimizers import Adam; from tensorflow.keras.models import Model; from tensorflow.keras.callbacks import ModelCheckpoint; import tensorflow_hub as hub'
+python -c 'import numpy as np; import pandas as pd; import tensorflow as tf; from tensorflow.keras.layers import Dense, Input; from tensorflow.keras.optimizers import Adam; from tensorflow.keras.models import Model; from tensorflow.keras.callbacks import ModelCheckpoint; import tensorflow_hub as hub; import dask; import dask.array as da; import dask.bag as db; import dask.dataframe as dd;'
 
 ## If you want to start jupter, do it
 # jupyter notebook --no-browser --port=8080
